5d131969d41defec3a81e980dbd5d92baf32d9db
vtmproject <dev.vtmproject@gmail.com>
2019-09-13 15:48:39 +0300

    add onesignal

--- www/js/onesignal.js
+++ www/js/onesignal.js
@@ -1,6 +1,6 @@
 document.addEventListener('deviceready', function () {
     // Enable to debug issues.
-    window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+    window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 6});
 
     var notificationOpenedCallback = function(jsonData) {
         console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
@@ -9,5 +9,21 @@ document.addEventListener('deviceready', function () {
     window.plugins.OneSignal
         .startInit("621980dd-98f7-433c-9605-52eeb053f12f")
         .handleNotificationOpened(notificationOpenedCallback)
+        .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
         .endInit();
+
+    window.plugins.OneSignal.addSubscriptionObserver(function (state) {
+        if (!state.from.subscribed && state.to.subscribed) {
+            console.log("Subscribed for OneSignal push notifications!")
+            // get player ID
+            state.to.userId
+        }
+        console.log("Push Subscription state changed: " + JSON.stringify(state));
+    });
+
+    window.plugins.OneSignal.getPermissionSubscriptionState(function(status) {
+        console.log(status);
+    });
+
+    window.plugins.OneSignal.provideUserConsent(true);
 }, false);
