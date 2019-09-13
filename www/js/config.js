0cc34587a7172ec0d233a2a4308da246b2929c00
vtmproject <dev.vtmproject@gmail.com>
2019-09-13 14:16:16 +0300

    Доработка сообщений

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var siteUrl = "http://127.0.0.1:8090";
+var config = {
+    loginUrl: siteUrl + "/mobileapp/login",
+    dataUrl: siteUrl + "/mobileapp/loadData",
+    meetingUrl: siteUrl + "/app#NewMeetingChart/meetingChart="
+};
