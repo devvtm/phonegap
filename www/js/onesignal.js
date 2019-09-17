492a0c5963806d06d73af46741261293f60c43ff
vtmproject <dev.vtmproject@gmail.com>
2019-09-17 17:14:43 +0300

    add onesignal

--- www/js/onesignal.js
+++ www/js/onesignal.js
@@ -2,9 +2,6 @@ document.addEventListener('deviceready', function () {
     // Enable to debug issues.
     window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
 
-    var notificationOpenedCallback = function(jsonData) {
-    };
-
     window.plugins.OneSignal
         .startInit("621980dd-98f7-433c-9605-52eeb053f12f")
         .handleNotificationOpened(notificationOpenedCallback)
