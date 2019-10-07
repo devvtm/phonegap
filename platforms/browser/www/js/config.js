d9115eb4258e1693f03e10d0a2d98ba82375734e
vtmproject <dev.vtmproject@gmail.com>
2019-10-07 14:43:51 +0300

    fix

--- platforms/browser/www/js/config.js
+++ platforms/browser/www/js/config.js
@@ -25,7 +25,7 @@ var CONFIG = {
             case "VTM":
             {
                 //this.siteUrl = "https://proj.vtm-dorproekt.ru:9443/";
-                this.siteUrl = "http://192.168.0.106:8090/";
+                this.siteUrl = "http://192.168.0.103:8090/";
                 break;
             }
             case "IKPAD":
