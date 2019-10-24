713baebf1ae590d69e4e24eabb22b7afdedf1efc
vtmproject <dev.vtmproject@gmail.com>
2019-10-24 17:51:46 +0300

    групповой чат

--- platforms/browser/www/js/config.js
+++ platforms/browser/www/js/config.js
@@ -39,8 +39,8 @@ var CONFIG = {
             {
                 case "VTM":
                 {
-                    //this.siteUrl = "https://proj.vtm-dorproekt.ru:9443/";
-                    this.siteUrl = "http://192.168.0.105:8090/";
+                    this.siteUrl = "https://proj.vtm-dorproekt.ru:9443/";
+                    //this.siteUrl = "http://192.168.0.105:8090/";
                     break;
                 }
                 case "IKPAD":
